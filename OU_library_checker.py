cb6300a998200bdc26b022964a724091cf84197f
Chimp56 <47754918+Chimp56@users.noreply.github.com>
2022-11-15 16:42:33 -0600

    Update OU_library_checker.py

--- OU_library_checker.py
+++ OU_library_checker.py
@@ -4,59 +4,45 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import streamlit as st
 
+# sets up the webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome('./chromedriver', chrome_options=options)
 
+# link to the reservations for Bizzel's Library rooms
 url = 'https://libcal.ou.edu/reserve/groupstudyrooms'
-# driver.get(url)
-
-
-def save_html(html, path):
-    with open(path, 'a') as f:
-        f.write(html)
-        
-        
-# save_html(r.content, 'google_com')
-
-def open_html(path):
-    with open(path, 'rb') as f:
-        return f.read()
     
-    
-# html = open_html('google_com')
-
-# more_buttons = driver.find_elements_by_class_name("moreLink")
-# for x in range(len(more_buttons)):
-#   if more_buttons[x].is_displayed():
-#       driver.execute_script("arguments[0].click();", more_buttons[x])
-#       time.sleep(1)
-def setBooking(driver):
+# retrieves the studyrooms bookings and returns it as a list
+def getBooking(driver):
+    # goes to the link
     driver.get(url)
+    # retrieves source code of the website
     page_source = driver.page_source
-
+    # parses the source code into something interactable
     soup = BeautifulSoup(page_source, 'xml')
-
+    # lists to store data in
     rooms = []
     times = []
+
+    # gets current booking status for every rooms
+    # each element in rooms is a list of times for each room
     rows = soup.find_all('div', class_='fc-timeline-events fc-scrollgrid-sync-inner')
     for row in rows:
         times = []
         time_slots = row.find_all('div', class_='fc-timeline-event-harness')
         for time_slot in time_slots:
-        # re
-        # print(review_selector.get('style', 'No title'))
             slot = time_slot.find('a')
             times.append(slot.get('title', 'no'))
-        # print(times, "\n\n")
         rooms.append(times)
     return rooms
 
+# determines whether library is busy or not and returns a list of of available rooms
 def is_library_busy(reservations):
     availableCount = 0
     avialable_places = []
+    # loops through the previously retrieved list to check for available rooms and time slots
     for i in range (len(reservations)):
         for j in range(3):
             try:
@@ -65,27 +51,33 @@ def is_library_busy(reservations):
                     availableCount += 1
             except:
                 pass
+    # not busy if there are 3 or more 30 minute time slots available in the next 1 and a half hours 
     if availableCount > 2:
         return "Not Busy", avialable_places        
     return "Busy", avialable_places
 
 
-# print(is_library_busy(reservations))
-
+# homepage = 'https://sites.google.com/d/1d4ciRnNkLIREEqSyKLK17wt8hy_KaqRU/p/1G0PwWL8LhKhYEe_2HUjWKM-NXLsgAGug/'
+t = st.empty()
+n = st.empty()
 while True:
-    t = st.empty()
     t.empty()
+    n.empty()
 
-    reservations = setBooking(driver)
+    reservations = getBooking(driver)
     is_busy, places = is_library_busy(reservations)
-    with t.container():
-        printString = is_busy + "\n\n"
-        # st.write(is_busy)
-        for i in places:
-            printString = printString + i + "\n"
-        t.write(printString)
+    
+    t.write(is_busy + "\n\n")
+    printString = ""
+    for i in places:
+        printString += "\n" + i + "\n\t\t   "
+    try:
+        n.write(printString)
+    except:
+        n.write("Little to no space available")
+    # link = '[Back to homepage](https://sites.google.com/d/1d4ciRnNkLIREEqSyKLK17wt8hy_KaqRU/p/1G0PwWL8LhKhYEe_2HUjWKM-NXLsgAGug/)'
+    # st.markdown(link, unsafe_allow_html=True)
     
     
     time.sleep(60)
-# setBooking(driver)
 
